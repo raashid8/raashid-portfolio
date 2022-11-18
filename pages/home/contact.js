90557b6b5e237e57b2cf72e30929b41faf8bd2db
Raashid <aaminaraashid@gmail.com>
2022-11-18 12:05:32 +0530

    Git add 5 R

--- pages/home/contact.js
+++ pages/home/contact.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  solid,
-  regular,
-  brands,
-  icon
-} from '@fortawesome/fontawesome-svg-core/import.macro'
+// import React from "react";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import {
+//   solid,
+//   regular,
+//   brands,
+//   icon
+// } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-const Contact = () => {
-  return (
-    <>
-      <div className="contact">
-        <a href="#" class="link">
-        <FontAwesomeIcon icon={solid('user-secret')} />
+// const Contact = () => {
+//   return (
+//     <>
+//       <div className="contact">
+//         <a href="#" class="link">
+//         <FontAwesomeIcon icon={solid('user-secret')} />
 
-        </a>
-        <a href="#" class="link">
-          <i class="fab fa-twitter twitter"></i>
-        </a>
-        <a href="#" class="link">
-          <i class="fab fa-whatsapp whatsapp"></i>
-        </a>
-        <a href="#" class="link">
-          <i class="fab fa-linkedin linkedin"></i>
-        </a>
-      </div>
-    </>
-  );
-};
+//         </a>
+//         <a href="#" class="link">
+//           <i class="fab fa-twitter twitter"></i>
+//         </a>
+//         <a href="#" class="link">
+//           <i class="fab fa-whatsapp whatsapp"></i>
+//         </a>
+//         <a href="#" class="link">
+//           <i class="fab fa-linkedin linkedin"></i>
+//         </a>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Contact;
+// export default Contact;
